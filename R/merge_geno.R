ccb6d383e119fd60a0c4e2e247c0f67a819c08d7
Scott Funkhouser <sfunkhouser@gmail.com>
2015-07-14 10:43:33 -0400

    improved method - finds common snps between n sets and merges using the common snps

--- R/merge_geno.R
+++ R/merge_geno.R
@@ -7,27 +7,19 @@
 #'  
 #' @param ... matrix of SNP array datasets
 #' @return matrix of merged SNP array data
+#' @import magrittr
 #' @export
 merge_geno <- function(...) {
-  
-  names_each <- c()
   
   # Put datasets in a list
   genos <- list(...)
   
-  # Get SNP order of first dataset
-  SNP_names <- colnames(genos[[1]])
+  # Find SNPs common among all datasets
+  common_snps <- lapply(genos, colnames) %>%
+                    Reduce(intersect, .)
   
-  # Is each dataset composed of the same SNPs?
-  name_check <-
-    lapply(genos, function(x) {
-      SNP_names %in% colnames(x) & length(SNP_names) == length(colnames(x))
-  })
-  if(!any(unlist(name_check))) stop("Datasets aren't compatible")
-  
-  # Make new list of datasets with SNPs arranged according
-  #   to the first
-  genos_new <- lapply(genos, function(x) x[, SNP_names])
+  # Produce new list of datasets composed of only common SNPs
+  genos_new <- lapply(genos, function(x) x[, common_snps])
   
   # Merge!
   do.call(rbind, genos_new)
